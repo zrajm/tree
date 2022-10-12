24b42feecf67cca949942d8c4f09b504cfbcebbd
zrajm <zrajm@klingonska.org>
2022-10-12 14:45:05 +0200

    Basic working version

---
+++ tree.js
@@ -0,0 +1,68 @@
+// -*- js-indent-level: 2 -*-
+// Copyright 2022 by zrajm. License: CC BY-SA (text), GPLv2 (code)
+
+// Invoke cb(tag) when elements matching CSS selector <sel> show up on screen.
+function onshow(sel, cb) {
+  const o = new IntersectionObserver(e => {
+    e.filter(e => e.isIntersecting).forEach(e => cb(e.target))
+  })
+  document.querySelectorAll(sel).forEach(t => o.observe(t))
+}
+
+function addLine(t, d, adj) {
+  t.insertAdjacentHTML('afterbegin', '<line>')
+  const line = t.firstChild
+  const [x1, y1] = getCoords(line)
+  const [x2, y2] = getCoords(d, adj)
+  line.style.height = Math.sqrt(((x2 - x1) ** 2) + ((y2 - y1) ** 2)) + 'px'
+  line.style.transform = `rotate(${(Math.atan2(y2 - y1, x2 - x1) * 180 / Math.PI) - 90}deg)`
+}
+
+// Returns relative coordinates. (The actual coords are not important as long
+// as all coords have the same origin, since we're just going to calculate the
+// line length and angle with these).
+function getCoords(t, { adjX = 0, adjY = 0 } = {}) {
+  const { left, top } = t.getBoundingClientRect()
+  return [left + adjX, top + adjY]
+}
+function drawTreeDiagram(tag) {
+  if (tag.classList.contains('drawn')) { return }  // already drawn
+  const nodes = tag.querySelectorAll(':scope node')
+  // Widen <node>s that aren't wide enough to accomodate its absolutely
+  // positioned label. (If label already fits, don't modify the width it
+  // because adjustment is actually quite SLOW.)
+  nodes.forEach(node => {
+    const labelWidth = parseInt(getComputedStyle(node, ':before')['width'])
+    if (node.clientWidth < labelWidth) {
+      node.style.boxSizing = 'content-box'
+      node.style.minWidth = labelWidth + 'px'
+    }
+  })
+  // Add <line>s.
+  nodes.forEach(node => {
+    const children = node.querySelectorAll(':scope>node')
+    node.classList.add(children.length ? 'branch' : 'leaf')
+    if (children.length) {  // branch node node
+      children.forEach(child => {
+        addLine(node, child, { adjX: child.offsetWidth / 2 })
+      })
+    } else {                // leaf node
+      const nodeStyle = getComputedStyle(node)
+      const adj = parseInt(getComputedStyle(node, ':before')['height']) +
+        parseInt(nodeStyle['border-bottom-width']) +
+        parseInt(nodeStyle['padding-bottom'])
+      addLine(node, node, {
+        adjX: node.offsetWidth / 2,
+        adjY: node.offsetHeight - adj,
+      })
+    }
+  })
+  tag.classList.add('drawn');
+}
+
+addEventListener('load', () => {
+  // Watch all <tree> tags, draw each tree when it become visible.
+  onshow('tree', drawTreeDiagram)
+})
+
+/*[eof]*/
